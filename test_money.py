195f75843e37d3fab049d9c695d8b5869507cc02
Christopher Barnes <senrabc@gmail.com>
2015-08-07 13:49:36 -0400

    clean up other tests

--- test_money.py
+++ test_money.py
@@ -13,7 +13,6 @@ class TestMoney(unittest.TestCase):
         self.assertEqual(money.get_amount(), 40)
 
     def test_add(self):
-<<<<<<< HEAD
         """ Verify that the add function works """
         money = Money(1)
         money.add(2)
@@ -22,32 +21,15 @@ class TestMoney(unittest.TestCase):
 
 
 
-        """
-        verify that we can add a number to the class amount
-        """
-        money = Money(1)
-        money.add(2)
-        self.assertEqual(3, money.get_amount())
-
-
-    def test_atloiaco_mulitply(self):
-        money = Money(2)
-        money.multiply(2)
-        self.assertEqual(1,1)
-
-    def test_nrejack_multiply(self):
-    	""" Verify that we can multiply the class amount by a number"""
-    	money = Money(10)
-    	money.multiply(3)
-
-    	self.assertEqual(30, money.get_amount())
 
     def test_cpb_multiply(self):
-        """ Verify that the multiply function works """
-        money = Money(3)
-        money.multiply(2)
+    	""" Verify that we can multiply """
+    	money = Money(16)
+    	money.multiply(12)
 
-        self.assertEqual(3, money.get_amount())
+    	self.assertEqual(192, money.get_amount())
+
+
 
 if __name__ == '__main__':
     unittest.main()
