6a5c21727dcea4b04a1abf26acd13ec827cc5abc
Taeber Rapczak <taeber@ufl.edu>
2015-08-07 13:43:32 -0400

    Merge

--- money.py
+++ money.py
@@ -13,9 +13,3 @@ class Money(object):
 
     def add(self, amt):
         self._amount = self._amount + amt
-<<<<<<< HEAD
-        
-    def multiply(self, amt):
-        self._amount = self._amount * amt
-=======
->>>>>>> fae5bfa99a80a6b0cdc9e2956faa7f1838f0bea3
