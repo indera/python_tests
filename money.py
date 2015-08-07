a7599b3ee39bc1569929d8ef49e03bf6c431e5a9
Roy Keyes <keyes@ufl.edu>
2015-08-07 14:04:21 -0400

    Add multiplication and division as well as some code cleanup

--- money.py
+++ money.py
@@ -1,15 +1,26 @@
 
 class Money(object):
 
-    """ Sample money object """
+    # Sample Money Object Instantiation
     def __init__(self, balance):
         self._amount = balance
 
-    def subtract(self, amt):
-        self._amount = self._amount - amt
-
+    # Accessor Methods for Class Variables
     def get_amount(self):
         return self._amount
 
+    # Addition
     def add(self, amt):
         self._amount = self._amount + amt
+
+    # Subtraction
+    def subtract(self, amt):
+        self._amount = self._amount - amt
+
+    # Multiplication
+    def multiply(self, amt):
+        self._amount = self._amount * amt
+
+    # Division
+    def divide(self, amt):
+        self._amount = self._amount / amt
--- test_money.py
+++ test_money.py
@@ -6,27 +6,35 @@ from money import Money
 
 class TestMoney(unittest.TestCase):
 
+    # Addition
+    def test_add(self):
+
+        money = Money(1)
+        money.add(2)
+
+        self.assertEqual(3, money.get_amount())
+
+    # Substraction
     def test_subtract(self):
-        """ Verify that the subtract function works well """
         money = Money(42)
         money.subtract(2)
 
         self.assertEqual(40, money.get_amount())
 
-        self.assertEqual(money.get_amount(), 40)
+    # Multiplication
+    def test_multiply(self):
 
-    def test_add(self):
-        """
-        verify that we can add a number to the class amount
-        """
-        money = Money(1)
-        money.add(2)
-        self.assertEqual(3, money.get_amount())
-
-    def test_keyes_multiply(self):
         money = Money(8)
         money.multiply(8)
+
         self.assertEqual(64, money.get_amount())
+
+    # Division
+    def test_divide(self):
+        money = Money(64)
+        money.divide(8)
+
+        self.assertEqual(8, money.get_amount())
 
     
 if __name__ == '__main__':
