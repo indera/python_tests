92573a1a29b884eaf02371d5231c5c5bc75f3cf0
Taeber Rapczak <taeber@ufl.edu>
2015-08-07 13:58:23 -0400

    Add test for multiply

--- test_money.py
+++ test_money.py
@@ -13,10 +13,18 @@ class TestMoney(unittest.TestCase):
         self.assertEqual(money.get_amount(), 40)
 
     def test_add(self):
-        """ Verify that the subtract function works well"""
+        """ Verify that the add function works well"""
         money = Money(42)
         money.add(2)
         self.assertEqual(money.get_amount(), 44)
+
+    def test_multiply(self):
+        """ Verify that the multiply function works well"""
+        money = Money(3)
+        money.multiply(10)
+        self.assertEqual(money.get_amount(), 30)
+
+
 
 if __name__ == '__main__':
     unittest.main()
