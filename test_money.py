3c9a8ab6b7b3b02e8bf7b47dfee2b893e9bb3fcc
Kevin Steven Hanson <kshanson@ufl.edu>
2015-08-07 14:09:36 -0400

    renamed kshanson file to test money

--- test_money.py
+++ test_money.py
@@ -5,66 +5,30 @@ from money import Money
 
 
 class TestMoney(unittest.TestCase):
-    def test_asura_multiply(self):
-        money = Money(2)
-        money.multiply(3)
-        self.assertEqual(6, money.get_amount())
 
     def test_subtract(self):
-        """ Verify that the subtract function works well """
+        """ Verify that the subtract function works well"""
         money = Money(42)
         money.subtract(2)
-
-        self.assertEqual(40, money.get_amount())
-
         self.assertEqual(money.get_amount(), 40)
-
-    def test_add(self):
-        """
-        verify that we can add a number to the class amount
-        """
-        money = Money(1)
-        money.add(2)
-        self.assertEqual(3, money.get_amount())
-
-    def test_atloiaco_mulitply(self):
-        money = Money(2)
-        money.multiply(2)
-        self.assertEqual(1,1)
-
-    def test_nrejack_multiply(self):
-        """ Verify that we can multiply the class amount by a number"""
-        money = Money(10)
-        money.multiply(3)
-
-        self.assertEqual(30, money.get_amount())
-
-    def test_taeber_multiple(self):
-        # Better have my money!
-        money = Money(3)
-        money.multiply(2)
-        self.assertEqual(money.get_amount(), 6)
-        money.multiply(1.0/6)
-        self.assertEqual(money.get_amount(), 1)
-
-    def test_cpb_multiply(self):
-        """ Verify that the multiply function works """
-        money = Money(3)
-        money.multiply(2)
-
-        self.assertEqual(3, money.get_amount())
 
     def test_add(self):
         """ Verify that we can add a number to the class amount"""
         money = Money(1)
         money.add(2)
         self.assertEqual(3, money.get_amount())
-        
-    def test_cavedivr_multiply(self):
-        """ Verify that we can multiply a number by the class amount"""
-        money = Money(29)
-        money.multiply(2)
-        self.assertEqual(58, money.get_amount())
-    
+
+    def test_multiply(self):
+        """ Verify that we can multiple a number to the class amount"""
+        money = Money(2)
+        money.multiply(3)
+        self.assertEqual(6, money.get_amount())
+
+    def test_divide(self):
+        """ Verify that we can multiple a number to the class amount"""
+        money = Money(4)
+        money.divide(2)
+        self.assertEqual(2, money.get_amount())
+
 if __name__ == '__main__':
     unittest.main()
