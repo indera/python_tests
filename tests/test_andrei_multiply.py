4307e3a2898c73bdcb43c74a21e2831e9790a0e2
Andrei Sura <sura.andrei@gmail.com>
2015-08-07 13:51:15 -0400

    rename

--- tests/test_andrei_multiply.py
+++ tests/test_andrei_multiply.py
@@ -5,7 +5,7 @@ from money import Money
 
 class TestMoneyAndrei(unittest.TestCase):
 
-    def test_asura_multiply(self):
+    def test_multiply(self):
         money = Money(2)
         money.multiply(3)
         self.assertEqual(6, money.get_amount())
